59d5c61a4b8ca86fa03c956b589e5872edfbca1e
orizzzar <70523219+orizzzar@users.noreply.github.com>
2021-11-25 15:30:21 +0100

    Finally

--- app/screens/QuestionScreen.js
+++ app/screens/QuestionScreen.js
@@ -2,64 +2,8 @@ import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
 import { BarCodeScanner } from "expo-barcode-scanner";
 import Ionicons from "@expo/vector-icons/Ionicons";
-
-
-
-/***********************************************************/
-/***********************************************************/
-/* Questions (there is a TODO with them)
-/***********************************************************/
-
-
-
-const questions = [
-  {
-    questionName: "APV",
-    QuestionLocation: "APV",
-    questionText: "What is the most common fish in around waters in ZEELAND",
-    answerOptions: [
-      { answerText: "Salmonela", isCorrect: false },
-      { answerText: "Carp", isCorrect: false },
-      { answerText: "Fluke", isCorrect: true },
-      { answerText: "Salmon", isCorrect: false },
-    ],
-  },
-  {
-    questionName: "HZ",
-    QuestionLocation: "HZ",
-    questionText:
-      "What is the tasties sea food in ZEELAND according to the articles?",
-    answerOptions: [
-      { answerText: "Measles", isCorrect: false },
-      { answerText: "Musels", isCorrect: true },
-      { answerText: "Muscules", isCorrect: false },
-      { answerText: "Fish", isCorrect: false },
-    ],
-  },
-  {
-    questionName: "AH",
-    QuestionLocation: "AH",
-    questionText: "Where is Gondola now?",
-    answerOptions: [
-      { answerText: "Knowhere", isCorrect: false },
-      { answerText: "Knowhere", isCorrect: false },
-      { answerText: "GERMANY", isCorrect: true },
-      { answerText: "Knowhere", isCorrect: false },
-    ],
-  },
-  {
-    questionName: "STREET",
-    QuestionLocation: "STREET",
-    questionText: "HOW WILL ARTIC MELTING ICE AFFECT THE ZEELANDS LAND?",
-    answerOptions: [
-      { answerText: "It will get flooded.", isCorrect: true },
-      { answerText: "Zeelands becomes desert.", isCorrect: false },
-      { answerText: "Nothing", isCorrect: false },
-      { answerText: "No Smoske for Deyan.", isCorrect: false },
-    ],
-  },
-];
-
+import { useAtom } from 'jotai'
+import tokensAtom from '../store/tokens.js'
 
 
 /***********************************************************/
@@ -88,6 +32,7 @@ const PermissionScreen = ({
 
 const ScanningScreen = ({
   isScanned,
+  isScanValid,
   handleBarCodeScanned,
   reset,
 }) => (
@@ -153,24 +98,24 @@ const ScoreView = ({
 
 
 const QuestionView = ({
-  question,
+  scannedToken,
   handleAnswerOptionClick,
   reset,
 }) => (
   <View>
     <View style={styles.questionViewTitle}>
       <Text style={styles.questionViewTitleText}>
-        {question.questionText}
+        {scannedToken.question}
       </Text>
     </View>
     <View style={styles.questionViewAnswerOptions}>
-      {question.answerOptions.map((answerOpt, index) => (
+      {scannedToken.answers.map((answer, index) => (
         <Button
           color="#ffffff"
           onPress={() =>
-            handleAnswerOptionClick(answerOpt.isCorrect)
+            handleAnswerOptionClick(answer.isCorrect)
           }
-          title={answerOpt.answerText}
+          title={answer.text}
           key={index}
         />
       ))}
@@ -197,14 +142,20 @@ const questionScreen = () => {
   const [isCameraAllowed, setIsCameraAllowed] = useState(null);
   const [isScanned, setIsScanned] = useState(false);
   const [isScanValid, setIsScanValid] = useState(false);
-  const [question, setQuestion] = useState(null);
   const [isAnswered, setIsAnswered] = useState(false);
   const [postAnswerMsg, setPostAnswerMsg] = useState("Not correct!");
-
+  const [tokens, setTokens] = useAtom(tokensAtom);
+  const [scannedName, setScannedName] = useState('');
 
   async function askForCameraPermission() {
     const { status } = await BarCodeScanner.requestPermissionsAsync();
     setIsCameraAllowed(status === "granted");
+  }
+
+  function trySetToken(name) {
+    if (!(name in tokens)) return false;
+    setScannedName(name);
+    return true;
   }
 
   function reset() {
@@ -214,37 +165,30 @@ const questionScreen = () => {
   }
 
   function handleBarCodeScanned({ data: scannedText }) {
+    setIsScanValid(trySetToken(scannedText));
     setIsScanned(true);
-
-    const question = questions.find(question =>
-      question.questionName === scannedText
-    );
-
-    if (question !== undefined) {
-      setQuestion(question);
-      setIsScanValid(true);
-    }
   }
 
   function handleAnswerOptionClick(isCorrect) {
     if (isCorrect) {
       setPostAnswerMsg("Your answer is correct!");
+      setTokens(prev => ({
+        ...prev, [scannedName]: {
+          ...tokens[scannedName],
+          isCollected: true,
+        }
+      }));
     } else {
       setPostAnswerMsg("Your answer is incorrect!");
     }
     setIsAnswered(true);
   }
 
-
-  /* I didnt find a better way 
-   * (not to this exact piece but in general, 
-   * passing all those props down and down feels wierd)
-   */
   const theProps = {
     isAnswered,
     postAnswerMsg,
     reset,
-    question,
+    scannedToken: tokens[scannedName],
     handleAnswerOptionClick,
     askForCameraPermission,
     isScanned,
@@ -253,7 +197,7 @@ const questionScreen = () => {
 
   useEffect(() => {
     askForCameraPermission();
-  });
+  }, []);
 
   if (!isCameraAllowed) {
     return (
@@ -270,7 +214,7 @@ const questionScreen = () => {
       <AfterScanScreen {...theProps} />
     );
   }
-
+  
 }
 
 export default questionScreen;
